967c8e37e05ca3fad98e15ed07f69ad12380156b
Bharat Pandey <bpaans.elx@gmail.com>
2016-10-23 21:35:04 -0500

    domain order address .... added

--- src/main/java/waa/project/domain/Order.java
+++ src/main/java/waa/project/domain/Order.java
@@ -1,5 +1,12 @@
 package waa.project.domain;
 
+import java.util.List;
+
 public class Order {
+	private int id;
+	private String customerId; // Customer domain
+	private Address shippingAddress;
+	private List<OrderLine> orderList;
+	
 
 }
